460253a65c3375ef83583eb5c7b2a67857ba26fa
newsworthy39 <newsworthy39@github.com>
2021-09-03 20:05:11 +0200

    Double-fork

--- main-gtk.cpp
+++ main-gtk.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 #include <qemu-hypervisor.hpp>
-#include <qemu-context.hpp>
 
 int main(int argc, char *argv[])
 {
     QemuContext ctx;
-    bool verbose = false;
-    std::string instanceargument = QEMU_DEFAULT_INSTANCE;
+    bool verbose = false, cleanup = false;
+    std::string instance = QEMU_DEFAULT_INSTANCE;
     std::string tapname = QEMU_DEFAULT_INTERFACE;
-    std::string imagedb = QEMU_DEFAULT_IMAGEDB;
-    std::string image = QEMU_DEFAULT_MACHINE;
+    std::string machine = QEMU_DEFAULT_MACHINE;
+    QEMU_DISPLAY display = QEMU_DISPLAY::GTK;
 
     for (int i = 1; i < argc; ++i)
     { // Remember argv[0] is the path to the program, we want from argv[1] onwards
 
         if (std::string(argv[i]).find("-h") != std::string::npos)
         {
-            std::cout << "Usage(): " << argv[0] << " (-h) (-v) -machine {default=" << QEMU_DEFAULT_INSTANCE << "} -interface {default=" << QEMU_DEFAULT_INTERFACE << "}  -image {default=" << QEMU_DEFAULT_MACHINE << "} -drive hdd (n+1)" << std::endl;
+            std::cout << "Usage(): " << argv[0] << " (-h) (-v) -q -instance {default=" << QEMU_DEFAULT_INSTANCE << "} -interface {default=" << QEMU_DEFAULT_INTERFACE << "}  -machine {default=" << QEMU_DEFAULT_MACHINE << "} -drive hdd (n+1)" << std::endl;
             exit(-1);
         }
 
@@ -25,9 +24,9 @@ int main(int argc, char *argv[])
             verbose = true;
         }
 
-        if (std::string(argv[i]).find("-machine") != std::string::npos && (i + 1 < argc))
+        if (std::string(argv[i]).find("-instance") != std::string::npos && (i + 1 < argc))
         {
-            instanceargument = argv[i + 1];
+            instance = argv[i + 1];
         }
 
         if (std::string(argv[i]).find("-interface") != std::string::npos && (i + 1 < argc))
@@ -35,24 +34,71 @@ int main(int argc, char *argv[])
             tapname = argv[i + 1];
         }
 
+        if (std::string(argv[i]).find("-machine") != std::string::npos && (i + 1 < argc))
+        {
+            machine = argv[i + 1];
+        }
+
         if (std::string(argv[i]).find("-drive") != std::string::npos && (i + 1 < argc))
         {
             QEMU_drive(ctx, argv[i + 1]);
         }
-
-        if (std::string(argv[i]).find("-image") != std::string::npos && (i + 1 < argc))
+        if (std::string(argv[i]).find("-q") != std::string::npos)
         {
-            image = argv[i + 1];
+            cleanup = true;
         }
     }
 
-    QEMU_instance(ctx, instanceargument);
-    QEMU_display(ctx, QEMU_DISPLAY::GTK);
-    QEMU_machine(ctx, image, imagedb);
+    std::string guestid = QEMU_instance(ctx, instance);
+    QEMU_display(ctx, display);
+    QEMU_machine(ctx, machine);
 
-    // if all is well, we could fork, here and setup a listener, for
-    // the qemu-sockets, we specify - reading back and forth.
-    QEMU_Launch(ctx, tapname);
+    // special case. If cleanup is false. QEMU_LAUNCH NEVER RETURNS!
+    if (cleanup == false)
+    {                                     //
+        QEMU_Launch(ctx, tapname, false); //
+    }
+
+    // lets double-fork. - fuck.
+    pid_t parent = fork();
+    if (parent == 0)
+    {
+
+        // fork and wait, return - then cleanup files.
+        pid_t pid = fork();
+        int status;
+        if (pid == 0)
+        {
+            QEMU_Launch(ctx, tapname, true); // where qemu-launch, BLOCKS.
+        }
+        else
+        {
+            do
+            {
+                pid_t w = waitpid(pid, &status, WUNTRACED | WCONTINUED);
+                if (WIFEXITED(status))
+                {
+                    std::string str_pid = "/tmp/" + guestid + ".pid";
+                    std::string str_mon = "/tmp/" + guestid + ".monitor";
+
+                    unlink(str_pid.c_str());
+                    unlink(str_mon.c_str());
+                }
+                else if (WIFSIGNALED(status))
+                {
+                    printf("killed by signal %d\n", WTERMSIG(status));
+                }
+                else if (WIFSTOPPED(status))
+                {
+                    printf("stopped by signal %d\n", WSTOPSIG(status));
+                }
+                else if (WIFCONTINUED(status))
+                {
+                    printf("continued\n");
+                }
+            } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+        }
+    } // leave parent.
 
     return EXIT_SUCCESS;
 }
