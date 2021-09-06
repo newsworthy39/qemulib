57a4ae54f915273d8d0ef51948308336dee557b3
newsworthy39 <newsworthy39@github.com>
2021-09-06 19:44:29 +0200

    Consolidated options into some clever old-tricky-stuff.

--- main-gtk.cpp
+++ main-gtk.cpp
@@ -5,11 +5,17 @@
 int main(int argc, char *argv[])
 {
     QemuContext ctx;
-    bool verbose = false;
+    bool verbose = false, keeplinkonexit = false;
+    std::string command(argv[0]);
     std::string instance = QEMU_DEFAULT_INSTANCE;
     std::string tapname = QEMU_DEFAULT_INTERFACE;
     std::string machine = QEMU_DEFAULT_MACHINE;
     QEMU_DISPLAY display = QEMU_DISPLAY::GTK;
+
+    if (command.find("-headless") != std::string::npos)
+    {
+        display = QEMU_DISPLAY::VNC;
+    }
 
     for (int i = 1; i < argc; ++i)
     { // Remember argv[0] is the path to the program, we want from argv[1] onwards
@@ -33,6 +39,7 @@ int main(int argc, char *argv[])
         if (std::string(argv[i]).find("-interface") != std::string::npos && (i + 1 < argc))
         {
             tapname = argv[i + 1];
+            keeplinkonexit = true;
         }
 
         if (std::string(argv[i]).find("-machine") != std::string::npos && (i + 1 < argc))
@@ -55,20 +62,19 @@ int main(int argc, char *argv[])
     if (parent == 0)
     {
 
+        if (tapname == QEMU_DEFAULT_INTERFACE)
+        {
+            tapname = QEMU_Allocate_Link("enp2s0");
+        }
+
         // fork and wait, return - then cleanup files.
         pid_t pid = fork();
         int status;
         if (pid == 0)
         {
-
+            QEMU_Open_Link(ctx, tapname);
             QEMU_Notify_Started(ctx);
-            
-            if (tapname == QEMU_DEFAULT_INTERFACE)
-            {
-                tapname = allocateLink("enp2s0");
-            }
-
-            QEMU_Launch(ctx, tapname, true); // where qemu-launch, BLOCKS.
+            QEMU_Launch(ctx, true); // where qemu-launch, BLOCKS.
         }
         else
         {
@@ -77,6 +83,10 @@ int main(int argc, char *argv[])
                 pid_t w = waitpid(pid, &status, WUNTRACED | WCONTINUED);
                 if (WIFEXITED(status))
                 {
+                    // if the interface, was supplied, keep-it-on-exit.
+                    if (keeplinkonexit == false)
+                        QEMU_Delete_Link(ctx, tapname);
+
                     QEMU_Notify_Exited(ctx);
                 }
                 else if (WIFSIGNALED(status))
