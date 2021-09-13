2d2e34f68920efaa98973f89973967a038bff862
newsworthy39 <newsworthy39@github.com>
2021-09-13 20:26:28 +0200

    Updated qemu-link.hpp

--- include/qemu-link.hpp
+++ include/qemu-link.hpp
@@ -2,23 +2,29 @@
 #define __QEMU_LINK_HPP__
 
 #include <netinet/ether.h>
-
 #include <netlink/netlink.h>
 #include <netlink/route/link.h>
 #include <netlink/route/link/macvtap.h>
+//#include <linux/if.h>
+#include <linux/if_tun.h>
 #include <linux/netlink.h>
 #include <net/if.h>
 #include <string>
 #include <iostream>
 #include <random>
 #include <memory>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
 #include <qemu-hypervisor.hpp>
 
-std::string QEMU_Allocate_Link(std::string masterinterface);
-void QEMU_Delete_Link(QemuContext &ctx, std::string interface);
-int QEMU_Open_Link(QemuContext &ctx, std::string interface);
-void QEMU_Close_Link(QemuContext &ctx, int linkfd);
+void QEMU_allocate_tun(QemuContext &ctx, std::string bridge);
+std::string QEMU_allocate_macvtap(QemuContext &ctx, std::string masterinterface);
+std::string getMacSys(std::string tapname);
 std::string QEMU_Generate_Link_Mac();
 std::string QEMU_Generate_Link_Name();
-
+void QEMU_Delete_Link(QemuContext &ctx, std::string interface);
+void QEMU_Close_Link(QemuContext &ctx, int linkfd);
+int QEMU_OpenQMPSocket(QemuContext &ctx);
 #endif
--- src/qemu-link.cpp
+++ src/qemu-link.cpp
@@ -14,6 +14,20 @@ std::string m3_string_format(const std::string &format, Args... args)
     return std::string(buf.get(), buf.get() + size - 1); // We don't want the '\0' inside
 }
 
+std::string getMacSys(std::string tapname)
+{
+    std::string mac;
+    ;
+    auto tapfile = m3_string_format("/sys/class/net/%s/address", tapname.c_str());
+    std::ifstream myfile(tapfile.c_str());
+    if (myfile.is_open())
+    {                  // always check whether the file is open
+        myfile >> mac; // pipe file's content into stream
+    }
+
+    return mac;
+}
+
 std::string QEMU_Generate_Link_Mac()
 {
     static std::random_device rd;
@@ -39,30 +53,30 @@ std::string QEMU_Generate_Link_Mac()
 
 std::string QEMU_Generate_Link_Name()
 {
-    static std::random_device rd;
-    static std::mt19937 gen(rd());
-    static std::uniform_int_distribution<> dis(0, 15);
+    static std::random_device r;
+    static std::default_random_engine e1(r());
+    static std::uniform_int_distribution<int> dis(0, 15);
 
     std::stringstream ss;
     int i = 0, count = 0;
-    //ss << std::hex;
-    for (i = 0; i < 4; i++)
+    ss << std::hex;
+    for (i = 0; i < 8; i++)
     {
-        ss << dis(gen);
+        ss << dis(e1);
     }
 
-    return m3_string_format("tap-%04x", ss.str());
+    return m3_string_format("tap-%s", ss.str().c_str());
 }
 
-std::string QEMU_Allocate_Link(std::string masterinterface)
+std::string QEMU_allocate_macvtap(QemuContext &ctx, std::string masterinterface)
 {
     struct rtnl_link *link;
     struct nl_cache *link_cache;
     struct nl_sock *sk;
     struct nl_addr *addr;
     int err, master_index;
-    const std::string mac = QEMU_Generate_Link_Mac();
-    const std::string link_name =  QEMU_Generate_Link_Name();
+    std::string mac = QEMU_Generate_Link_Mac();
+    std::string link_name = QEMU_Generate_Link_Name();
 
     sk = nl_socket_alloc();
     if ((err = nl_connect(sk, NETLINK_ROUTE)) < 0)
@@ -107,16 +121,9 @@ std::string QEMU_Allocate_Link(std::string masterinterface)
         printf("rtnl_link_get_name failed\n");
     }
 
-    std::string tap(pchLinkName);
-
-    return tap;
-}
-
-int QEMU_Open_Link(QemuContext &ctx, std::string tapname)
-{
     // Finally, open the tap, before turning into a qemu binary, launching
     // the hypervisor.
-    auto tappath = m3_string_format("/dev/tap%d", if_nametoindex(tapname.c_str()));
+    auto tappath = m3_string_format("/dev/tap%d", if_nametoindex(pchLinkName));
     int fd = open(tappath.c_str(), O_RDWR);
     if (fd == -1)
     {
@@ -124,11 +131,18 @@ int QEMU_Open_Link(QemuContext &ctx, std::string tapname)
         exit(-1);
     }
 
-    std::cout << "Using network-device: " << tappath << ", mac: " << getMacSys(tapname) << std::endl;
+    std::cout << "Using network-device: " << link_name << ", mac: " << getMacSys(link_name) << std::endl;
     PushArguments(ctx, "-netdev", m3_string_format("tap,id=guest0,fd=%d,vhost=on", fd));
-    PushArguments(ctx, "-device", m3_string_format("virtio-net,mac=%s,netdev=guest0,id=internet-dev", getMacSys(tapname).c_str()));
+    PushArguments(ctx, "-device", m3_string_format("virtio-net,mac=%s,netdev=guest0,id=internet-dev", getMacSys(link_name).c_str()));
 
-    return fd;
+    return link_name;
+}
+
+void QEMU_allocate_tun(QemuContext &ctx, std::string bridge)
+{
+    std::cout << "Using network-device: /dev/net/tun" << std::endl;
+    PushArguments(ctx, "-netdev", m3_string_format("tap,id=guest0,script=no,downscript=no,br=%s,vhost=on", bridge));
+    PushArguments(ctx, "-device", "virtio-net,netdev=guest0,id=internet-dev"); //, getMacSys(dest).c_str()));
 }
 
 void QEMU_Close_Link(QemuContext &ctx, int linkfd)
@@ -158,4 +172,33 @@ void QEMU_Delete_Link(QemuContext &ctx, std::string interface)
 
     rtnl_link_put(link);
     nl_close(sk);
+}
+
+int QEMU_OpenQMPSocket(QemuContext &ctx)
+{
+    // Connect to unix-qmp-socket
+    int s, t, len;
+    struct sockaddr_un remote;
+    char str[4096];
+
+    std::string guestid = QEMU_Guest_ID(ctx); // Returns an UUIDv4.
+    std::string str_qmp = m3_string_format("/tmp/%s.socket", guestid.c_str());
+
+    if ((s = socket(AF_UNIX, SOCK_STREAM, 0)) == -1)
+    {
+        perror("socket");
+        exit(1); // TODO: Gracefull handling, of error.
+    }
+
+    remote.sun_family = AF_UNIX;
+    strcpy(remote.sun_path, str_qmp.c_str());
+    len = strlen(remote.sun_path) + sizeof(remote.sun_family);
+    if (connect(s, (struct sockaddr *)&remote, len) == -1)
+    {
+        std::cerr << "Could not connect to QMP-socket: " << str_qmp << std::endl;
+        exit(-1);
+    }
+    t = send(s, "{ \"execute\": \"qmp_capabilities\"} ", strlen("{ \"execute\": \"qmp_capabilities\"} "), 0);
+    t = recv(s, str, 4096, 0);
+    return s;
 }
