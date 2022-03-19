d63c4f839c35c9b1ff578a44c2c1032ba85aee85
newsworthy39 <newsworthy39@github.com>
2022-03-19 22:12:28 +0100

    Locking allows for repetable execution

--- scripts/bootnetwork.sh
+++ scripts/bootnetwork.sh
@@ -5,5 +5,7 @@ if [ "$EUID" -ne 0 ]
 fi
 
 
-sudo examples/build/qemu -headless -model c1-smaller -network default -profile dhcp instance://dhcp-1234
-# build/qemu -headless -model c1-faster -network default instance://testing-1234
+sudo build/qemu -headless -model c1-smaller -network default -profile dhcp instance://dhcp-1234
+sudo build/qemu -headless -model c1-faster  -network default -profile ami  instance://ami-1234
+sudo build/qemu -headless -model c1-faster  -network default -profile ami  instance://ami-1235
+sudo build/qemu -headless -model c1-faster  -network default -profile ami  instance://ami-1236
