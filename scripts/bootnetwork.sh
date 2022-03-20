e41bced6c474449d6fe635bb4230d553400b6d44
newsworthy39 <newsworthy39@github.com>
2022-03-20 16:49:37 +0100

    Changed for ephimeral

--- scripts/bootnetwork.sh
+++ scripts/bootnetwork.sh
@@ -5,6 +5,6 @@ if [ "$EUID" -ne 0 ]
 fi
 
 sudo build/qemu -headless -model c1-smaller -network default -profile dhcp instance://dhcp-1234
-sudo build/qemu -headless -model c1-faster -snapshot -network default -profile ami  instance://ami-1234
-sudo build/qemu -headless -model c1-faster -snapshot -network default -profile ami  instance://ami-1235
-sudo build/qemu -headless -model c1-faster -snapshot -network default -profile ami  instance://ami-1236
+sudo build/qemu -headless -model c1-faster -ephimeral -network default -profile ami  instance://ami-1234
+sudo build/qemu -headless -model c1-faster -ephimeral -network default -profile ami  instance://ami-1235
+sudo build/qemu -headless -model c1-faster -ephimeral -network default -profile ami  instance://ami-1236
