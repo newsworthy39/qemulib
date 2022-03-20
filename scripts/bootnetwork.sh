e96bd6a2a8e18bb8cf66e67a562bbb4a29b0d530
newsworthy39 <newsworthy39@github.com>
2022-03-20 15:53:29 +0100

    Added snapshot-mode for test-guests

--- scripts/bootnetwork.sh
+++ scripts/bootnetwork.sh
@@ -4,8 +4,7 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-
 sudo build/qemu -headless -model c1-smaller -network default -profile dhcp instance://dhcp-1234
-sudo build/qemu -headless -model c1-faster  -network default -profile ami  instance://ami-1234
-sudo build/qemu -headless -model c1-faster  -network default -profile ami  instance://ami-1235
-sudo build/qemu -headless -model c1-faster  -network default -profile ami  instance://ami-1236
+sudo build/qemu -headless -model c1-faster -snapshot -network default -profile ami  instance://ami-1234
+sudo build/qemu -headless -model c1-faster -snapshot -network default -profile ami  instance://ami-1235
+sudo build/qemu -headless -model c1-faster -snapshot -network default -profile ami  instance://ami-1236
