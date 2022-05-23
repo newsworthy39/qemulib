e88ad47c9563094a20c059d59466a34732f4d452
newsworthy39 <newsworthy39@github.com>
2022-05-23 20:36:21 +0200

    Make instances not volatile

--- scripts/bootmqtt.sh
+++ scripts/bootmqtt.sh
@@ -5,4 +5,5 @@ if [ "$EUID" -ne 0 ]
 fi
 
 # sudo build/qemu -headless -model c1-smaller -network default -profile dhcp -vol slow:16g instance://dhcp-1234
-sudo build/qemu -headless -ephimeral -model t1-large -network macvtap -profile mqtt instance://mqtt-1234
+sudo build/qemu -headless -model t1-large -network macvtap -profile mqtt instance://mqtt-1234
+sudo build/qemu -headless -model t1-large -network macvtap -profile mqtt instance://mqtt-1235
