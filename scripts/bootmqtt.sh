2fcf4bb005c451946bb318e4884fb5bc6d1ca7e7
newsworthy39 <newsworthy39@github.com>
2022-05-23 15:47:30 +0200

    Typo

--- scripts/bootmqtt.sh
+++ scripts/bootmqtt.sh
@@ -4,5 +4,5 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-sudo build/qemu -headless -model c1-smaller -network default -profile dhcp -vol slow:16g instance://dhcp-1234
-sudo build/qemu -headless -model t1-large -network default -profile mqtt instance://mqtt-1234
+# sudo build/qemu -headless -model c1-smaller -network default -profile dhcp -vol slow:16g instance://dhcp-1234
+sudo build/qemu -headless -ephimeral -model t1-large -network macvtap -profile mqtt instance://mqtt-1234
